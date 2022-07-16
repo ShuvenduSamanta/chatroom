7e54df2953553ec081185a0505d4303abc425d32
ShuvenduSamanta <shuvendu247@gmail.com>
2022-07-16 15:00:30 +0530

    port setup & user front end

--- src/main/java/com/chat/app/controllers/MessageController.java
+++ src/main/java/com/chat/app/controllers/MessageController.java
@@ -1,9 +1,10 @@
 package com.chat.app.controllers;
 
-import com.chat.app.models.*;
+import com.chat.app.models.*;	
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +14,7 @@ public class MessageController {
 	@MessageMapping("/message")
 	@SendTo("/topic/return-to") //to subscribe to receive messages
 	public Message getContent(@RequestBody Message message) {
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+
 		return message;
 		
 	}
